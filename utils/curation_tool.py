026ba361bd81eebe0c5ed5492a34d824b4f48c5f
dikology <dgimaletdinov@gmail.com>
2024-12-29 17:48:03 +0300

    updated relevance

--- utils/curation_tool.py
+++ utils/curation_tool.py
@@ -17,6 +17,13 @@ for idx, row in df.iterrows():
         #readable_name = st.text_input("Human-readable name", value=row['human_readable_name'], key=f"name_{idx}")
         #description = st.text_area("Description", value=row['description'], key=f"description_{idx}")
         #relevance = st.checkbox("Mark as Relevant", value=row['relevance'], key=f"relevance_{idx}")
+        #relevance_options = ["Relevant", "Not Relevant", "Undecided"]  # Add options as needed
+        #relevance = st.selectbox(
+        #    "Relevance",
+        #    options=relevance_options,
+        #    index=relevance_options.index(row['relevance']) if row['relevance'] in relevance_options else 0,
+        #    key=f"relevance_{idx}"
+        #)
 
         # Update logic
         if st.button("Save", key=f"save_{idx}"):
